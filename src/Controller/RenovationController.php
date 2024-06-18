7e0e2a9bd747069541333a0efdc6b0ec00d345a1
Cabolsky <pierrehlauga@gmail.com>
2024-06-18 14:39:30 +0200

    MAJ RF

---
+++ src/Controller/RenovationController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RenovationController extends AbstractController
+{
+    #[Route('/renovation', name: 'app_renovation')]
+    public function index(): Response
+    {
+        return $this->render('renovation/index.html.twig', [
+            'controller_name' => 'RenovationController',
+            'title' => 'renovation',
+        ]);
+    }
+}
