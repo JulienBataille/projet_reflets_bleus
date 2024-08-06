1b6481639a31769fa94d3e58a84d16aed609adb7
landesprit <julien.bataille40@gmail.com>
2024-08-06 15:11:21 +0200

    MAJ

--- src/Controller/Admin/CataloguesCrudController.php
+++ src/Controller/Admin/CataloguesCrudController.php
@@ -3,7 +3,6 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Catalogues;
-
 use Vich\UploaderBundle\Form\Type\VichFileType;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
@@ -16,30 +15,30 @@ class CataloguesCrudController extends AbstractCrudController
     {
         return Catalogues::class;
     }
-    
 
     public function configureFields(string $pageName): iterable
     {
+        yield TextField::new('title', 'Titre');
 
-            yield TextField::new('title', 'Titre');
-            yield ImageField::new('thumbnailImage', 'Image miniature')
-            ->setBasePath('uploads/catalogues/thumbnails')
-            ->setUploadDir('public/uploads/catalogues/thumbnails')
-            ->setRequired(false);
-            // Champ pour télécharger le fichier PDF
-            yield TextField::new('PDFFile', 'Choisissez le PDF')
-                ->setFormType(VichFileType::class)
-                ->onlyOnForms();
+        // Champ pour afficher l'image miniature
+        yield ImageField::new('thumbnailImage', 'Image miniature')
+            ->setBasePath('uploads/catalogues/thumbnails')  // Chemin où les images sont accessibles
+            ->setUploadDir('public/uploads/catalogues/thumbnails')  // Chemin d'upload
+            ->setRequired(false); // Optionnel
 
-            // Champ pour afficher le nom du fichier PDF dans la liste
-            yield TextField::new('PDF', 'Nom du PDF')
-                ->onlyOnIndex()
-                ->formatValue(function ($value) {
-                    return $value ? sprintf('<a href="/uploads/catalogues/pdf/%s" target="_blank">%s</a>', $value, $value) : 'Aucun PDF';
-                });
-            
-            yield BooleanField::new('is_visible', 'Visible');
-        
+        // Champ pour télécharger le fichier PDF
+        yield TextField::new('PDFFile', 'Choisissez le PDF')
+            ->setFormType(VichFileType::class)
+            ->onlyOnForms();
+
+        // Champ pour afficher le nom du fichier PDF dans la liste
+        yield TextField::new('PDF', 'Nom du PDF')
+            ->onlyOnIndex()
+            ->formatValue(function ($value) {
+                return $value ? sprintf('<a href="/uploads/catalogues/pdf/%s" target="_blank">%s</a>', $value, $value) : 'Aucun PDF';
+            });
+
+        // Champ pour afficher et modifier la visibilité
+        yield BooleanField::new('is_visible', 'Visible');
     }
-
 }
