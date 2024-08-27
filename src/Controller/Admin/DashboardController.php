e48415af024d12488d28cbc489da244bcf44d24c
Cabolsky <pierrehlauga@gmail.com>
2024-08-27 11:40:58 +0200

    MAJ SECURITY 27/08

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -18,7 +18,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
 {
-    #[Route('/admin', name: 'admin')]
+    #[Route('/admin_854763', name: 'admin')]
     public function index(): Response
     {
         return $this->render('admin/dashboard.html.twig');
